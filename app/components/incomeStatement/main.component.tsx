7fa878495c8f2194c1eb3deb9759b2c3631535c5
auliahanifan <auliarahman80@gmail.com>
2023-01-08 05:45:13 +0700

    refactor: apply formatter

--- app/components/incomeStatement/main.component.tsx
+++ app/components/incomeStatement/main.component.tsx
@@ -12,17 +12,19 @@ import {
 import { useNavigate } from "@remix-run/react";
 import type { FC, ReactNode } from "react";
 import { useState } from "react";
-import type { InsightDto } from "~/dtos/insight.dto";
 import type { TableDto } from "~/dtos/table.dto";
 import IncomeStatementTableComponent from "./table.component";
 
 type IncomeStatementMainComponentProps = {
-table: TableDto,
-indexTab: number,
-children?: ReactNode
-}
-const IncomeStatementMainComponent: FC<IncomeStatementMainComponentProps> = (props) => {
-  const [table, _] = useState<TableDto>(props.table);
+  table: TableDto;
+  indexTab: number;
+  children?: ReactNode;
+};
+
+const IncomeStatementMainComponent: FC<IncomeStatementMainComponentProps> = (
+  props
+) => {
+  const [table, setTable] = useState<TableDto>(props.table);
   const [indexTab, setIndexTab] = useState<number>(props?.indexTab);
   let navigate = useNavigate();
 
