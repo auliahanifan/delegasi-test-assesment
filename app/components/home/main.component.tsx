2a2eb3addcc369c18e7071d4bf98a9b285ed4f61
auliahanifan <auliarahman80@gmail.com>
2023-01-07 15:20:07 +0700

    feat: add home page

--- app/components/home/main.component.tsx
+++ app/components/home/main.component.tsx
@@ -1,11 +1,44 @@
-import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
+import { ArrowForwardIcon } from "@chakra-ui/icons";
+import { Box, Button, Center, Heading } from "@chakra-ui/react";
+import { useNavigate } from "@remix-run/react";
 import { FC, ReactNode } from "react";
 
 const HomeMainComponent: FC<{ children?: ReactNode }> = () => {
+  let navigate = useNavigate();
+
   return (
-    <div>
-      <p>Ini Home</p>
-    </div>
+    <Box margin="5px 0px 40px 0px">
+      <Box justifyContent="center">
+        <Heading as="h4" size="md" textAlign="center" margin="10px 0px">
+          Beranda
+        </Heading>
+        <Heading as="h6" size="xs" textAlign="center" margin="10px 0px">
+          Hai! Yuk cek laporan keuangan sekarang.
+        </Heading>
+        <Center margin="10px 0px">
+          <Button
+            rightIcon={<ArrowForwardIcon />}
+            colorScheme="teal"
+            variant="outline"
+            textAlign="center"
+            onClick={() => navigate("/balance-sheet")}
+          >
+            Cek Laporan Neraca
+          </Button>
+        </Center>
+        <Center>
+          <Button
+            rightIcon={<ArrowForwardIcon />}
+            colorScheme="teal"
+            variant="outline"
+            textAlign="center"
+            onClick={() => navigate("/income-statement")}
+          >
+            Cek Laporan Laba Rugi
+          </Button>
+        </Center>
+      </Box>
+    </Box>
   );
 };
 
